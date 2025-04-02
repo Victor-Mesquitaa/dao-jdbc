4a8cd095095a16b78941ac3f18c9dd55cf396a82
Victor Mesquita <luizvictor2513@gmail.com>
2025-04-02 12:49:03 -0300

    Complete Project

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 import model.entities.Department;
 
-public interface DepartmenteDao {
-	
+public interface DepartmentDao {
+
 	void insert(Department obj);
 	void update(Department obj);
-	void deleteById (Integer id);
-	void findById (Integer id);
-	List <Department> findAll();
-	
-
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
 }
