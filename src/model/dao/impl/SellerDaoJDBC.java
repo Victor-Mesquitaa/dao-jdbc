563d89c1fe1b7471923033b507e526b4fde5d255
Victor Mesquita <luizvictor2513@gmail.com>
2025-04-02 13:02:34 -0300

    Projeto completo atualizado

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -97,7 +97,12 @@ public class SellerDaoJDBC implements SellerDao {
 			
 			st.setInt(1, id);
 			
-			st.executeUpdate();
+			int rows = st.executeUpdate();
+			
+			if (rows == 0 ) {
+				throw new DbException("Seller not exists");
+			}
+			
 		}
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
